a828dca82ce1e2d807c30180f6c914d6d3736007
alexandr.pankratov <alexandr.pankratov@skbkontur.ru>
2019-04-24 18:48:39 +0500

    Кекфайт

---
+++ Math .net Core/Infra/Config/ConfigFactory.cs
@@ -0,0 +1,18 @@
+namespace Math_.net_Core.Math
+{
+    public static class ConfigFactory
+    {
+        
+    }
+
+    public class FabricConfig
+    {
+        public double[] Picks;
+        public StartProfile[] StartProfiles;
+        public double[] NoiseAmps;
+        public IntegratorType[] IntegratorTypes;
+        public double[] Du;
+        
+
+    }
+}
